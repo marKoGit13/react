f7acf0c0760035d8059e3c71fe466846923e051b
I21426 <I21426@lab.aca>
2025-09-22 11:07:32 -0500

    Segundo cambio

--- src/Titulo.tsx
+++ src/Titulo.tsx
@@ -1,8 +1,42 @@
 
-const Titulo = () => {
-    return <h1 style={ { textAlign : "center" } }>
-        Login
+const Titulo = (props: TituloProps) => {
+
+    return <h1 style={ 
+        { 
+        textAlign : "center", 
+        backgroundColor: props.color != undefined ? props.color :"" 
+        } 
+        }>
+            
+        {
+            props.texto
+        }
     </h1>
+    
+
+    /*if (props.color != undefined){
+        //me estan pasando un color
+        return <h1 style={ { textAlign : "center", backgroundColor: props.color } 
+        }>
+        {
+            props.texto
+        }
+    </h1>
+    }else{
+        return <h1 style={ { textAlign : "center" } }>
+        {
+            props.texto
+        }
+    </h1>
+    }*/
+    
 }
+
+interface TituloProps{
+    texto : string
+    color? : string
+}
+
+
 export default Titulo
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import LoginPage from './LoginPage'
+//import LoginPage from './LoginPage'
 
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import LoginPage from './LoginPage'
 
 //dffds
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <LoginPage />
+    <LoginPage/>
   </StrictMode>,
 )
