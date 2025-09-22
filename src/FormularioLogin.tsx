ddc293339b134e1d7229faa56b19df0096a5937c
I21426 <I21426@lab.aca>
2025-09-22 12:26:47 -0500

    ultimo cambio de la clase del 22/09/2025

--- src/FormularioLogin.tsx
+++ src/FormularioLogin.tsx
@@ -1,13 +1,35 @@
+import { useState } from "react"
+
 const FormularioLogin = () => {
+
+    const [ userName, setUserName ] = useState<string>("")
+    const [ password, setPassword ] = useState<string>("")
+
+    const handleUserNameoOnChange = ( e : React.ChangeEvent<HTMLInputElement>) => {
+        setUserName(e.currentTarget.value)
+    }
+
+    const handleUserPasswordOnChange = (e : React.ChangeEvent<HTMLInputElement> ) => {
+        setPassword(e.currentTarget.value)
+    }
+
     return <form>
         <div>
             <label className="form-label">Usuario:</label>
-            <input className="form-control" type='text' />
+            <input className="form-control" 
+                type='text' 
+                value={ userName }
+                onChange={handleUserNameoOnChange}/>
         </div>
+
         <div>
             <label className="form-label">Password:</label>
-            <input className="form-control" type='password' />
+            <input className="form-control"
+                 type='password' 
+                 value={ password }
+                 onChange={handleUserPasswordOnChange}/>
         </div>
+
         <button className="btn btn-success w-100 mt-3" 
             type='button'>
                 Ingresar
